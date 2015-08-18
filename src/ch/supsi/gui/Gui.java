7d553e312128134539e6dcd993a484eb6790c080
SASCHA <domingus82@gmail.com>
2015-08-18 20:15:43 +0200

    Modificata GUI

--- src/ch/supsi/gui/Gui.java
+++ src/ch/supsi/gui/Gui.java
@@ -8,9 +8,12 @@ import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import ch.supsi.MailCrawler;
 import ch.supsi.SiteCrawler;
 
 import javax.swing.*;
@@ -24,41 +27,69 @@ public class Gui extends JPanel implements ActionListener  {
 	protected JTextArea textArea;
 	private String startUrl;
 	private final static String newline = "\n";
+	private Boolean isEmailSearchActive = true;
+	private Boolean isSiteSearchActive = true;
 
-	
+
 	private void addComponentsToPane(Container pane){
-		
+
+		//Header
 		BorderLayout headerLayout = new BorderLayout();
 		JPanel headerPanel = new JPanel(headerLayout);
-		
-			
+		pane.add(headerPanel, BorderLayout.PAGE_START);
+		//		
+		//Search Panel	
 		FlowLayout searchPaneLayout = new FlowLayout();
 		JPanel searchPanel = new JPanel(searchPaneLayout);
-		pane.add(searchPanel,  BorderLayout.PAGE_START);
-		
+		headerPanel.add(searchPanel,  BorderLayout.PAGE_START);
+		//		
 		textField = new JTextField(35);
 		textField.addActionListener(this);
 		textField.setText(startUrl);
 		searchPanel.add(textField);
-		
-		
+
 		JButton button = new JButton("Search");
-        searchPanel.add(button);
-        button.addActionListener(this);
-        
+		searchPanel.add(button);
+		button.setActionCommand("SEARCH");
+		button.addActionListener(this);
+
+		//Option Panel
+		FlowLayout optionLayout = new FlowLayout();
+		JPanel optionPanel = new JPanel(optionLayout);
+		headerPanel.add(optionPanel,  BorderLayout.CENTER);
+
+		JCheckBox emailCheck;
+		JCheckBox websiteCheck;
+
+		emailCheck = new JCheckBox("email");
+		emailCheck.setMnemonic(KeyEvent.VK_C);
+		emailCheck.setSelected(true);
+		emailCheck.setActionCommand("EMAIL");
+		emailCheck.addActionListener(this);
+
+		websiteCheck = new JCheckBox("web site");
+		websiteCheck.setMnemonic(KeyEvent.VK_C);
+		websiteCheck.setSelected(true);
+		websiteCheck.setActionCommand("SITE");
+		websiteCheck.addActionListener(this);
+
+		optionPanel.add(emailCheck);
+		optionPanel.add(websiteCheck);
+
+		//Text Area
 		textArea = new JTextArea(5, 20);
 		textArea.setEditable(false);
 		JScrollPane scrollPane = new JScrollPane(textArea);
 		scrollPane.setPreferredSize(new Dimension(200, 100));
 		pane.add(scrollPane, BorderLayout.CENTER);
 
-        JLabel empty = new JLabel("   ");
-        JLabel empty1 = new JLabel("   ");
-        JLabel empty2 = new JLabel("   ");
-        
-        pane.add(empty1, BorderLayout.LINE_START);
-        pane.add(empty2, BorderLayout.LINE_END);
-        pane.add(empty, BorderLayout.PAGE_END);
+		JLabel empty = new JLabel("   ");
+		JLabel empty1 = new JLabel("   ");
+		JLabel empty2 = new JLabel("   ");
+
+		pane.add(empty1, BorderLayout.LINE_START);
+		pane.add(empty2, BorderLayout.LINE_END);
+		pane.add(empty, BorderLayout.PAGE_END);
 	}
 
 	private void createAndShowGUI() {
@@ -67,7 +98,7 @@ public class Gui extends JPanel implements ActionListener  {
 		JFrame frame = new JFrame("HelloWorldSwing");
 		frame.setTitle("Crawler");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		addComponentsToPane(frame.getContentPane());
 
 		// Display the window.
@@ -75,46 +106,118 @@ public class Gui extends JPanel implements ActionListener  {
 		frame.setSize(550, 200);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-
 	}
 
 	public void actionPerformed(ActionEvent evt) {
-		String site = textField.getText();
 		
+
+		if (evt.getActionCommand() == "SEARCH") {
+			search();
+		} else if(evt.getActionCommand() == "SITE"){
+			if(isSiteSearchActive){
+				isSiteSearchActive = false;
+			}else{
+				isSiteSearchActive = true;
+			}
+			System.out.println("site "+isSiteSearchActive);
+		}else if(evt.getActionCommand() == "EMAIL"){
+			if(isEmailSearchActive){
+				isEmailSearchActive = false;
+			}else{
+				isEmailSearchActive = true;
+			}
+			System.out.println("email "+isEmailSearchActive);
+		}
+	}
+
+	private void search(){
 		try {
 			//Reset DataBase before new search
 			Main.db.runSql2("TRUNCATE Record;");
-			
-			SiteCrawler siteCrawler = new SiteCrawler(Main.db);
-			siteCrawler.setRecursiveOn(100);
-			siteCrawler.processPage(site);
-			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if(isSiteSearchActive){
+			searchSite();
+		}
+		if(isEmailSearchActive){
+			searchEmail();
+		}
+	}
+	
+	private void searchEmail(){
+		String site = textField.getText();
+
+		try {
+			//Reset DataBase before new search
+			//Main.db.runSql2("TRUNCATE Record;");
+
+			MailCrawler mailCrawler = new MailCrawler(Main.db);
+			//mailCrawler.setRecursiveOn(100);
+			mailCrawler.processPage(site);
+
 		} catch (SQLException | IOException e) {
 			e.printStackTrace();
 		}
 		textArea.append(site+ newline);
-		
-//		Getting the result sites form teh db and append them to the text area
+
+		//		Getting the result sites form teh db and append them to the text area
 		String sql = "select * from Record";
 		try {
 			ResultSet rs = Main.db.runSql(sql);
 			while(rs.next()){
-			//	String t =  rs.getString("URL");
+				//	String t =  rs.getString("URL");
 				textArea.append(rs.getString("URL")+ newline);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		//textField.selectAll();
+		//Make sure the new text is visible, even if there
+		//was a selection in the text area.
+		textArea.setCaretPosition(textArea.getDocument().getLength());	
+	}
+	private void searchSite(){
 		
-		
+		String site = textField.getText();
+
+		try {
+			//Reset DataBase before new search
+			//Main.db.runSql2("TRUNCATE Record;");
+
+			SiteCrawler siteCrawler = new SiteCrawler(Main.db);
+			siteCrawler.setRecursiveOn(100);
+			siteCrawler.processPage(site);
+
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+		}
+		textArea.append(site+ newline);
+
+		//		Getting the result sites form teh db and append them to the text area
+		String sql = "select * from Record";
+		try {
+			ResultSet rs = Main.db.runSql(sql);
+			while(rs.next()){
+				//	String t =  rs.getString("URL");
+				textArea.append(rs.getString("URL")+ newline);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
 		//textField.selectAll();
 
 		//Make sure the new text is visible, even if there
 		//was a selection in the text area.
 		textArea.setCaretPosition(textArea.getDocument().getLength());
+		
 	}
-	
+
 	public Gui(String URL){
 		super(new GridBagLayout());
 		this.startUrl = URL;
